6dfbe9557620b140dd17c855823efa25ff6c7229
Wanderson Fagundes Ramos <wandersonfagundesramos@gmail.com>
2021-06-17 11:19:28 -0300

    Obtendo um usuário por id

--- src/main/java/com/wfr/projetoworkshopmongodb/services/UserService.java
+++ src/main/java/com/wfr/projetoworkshopmongodb/services/UserService.java
@@ -1,12 +1,14 @@
 package com.wfr.projetoworkshopmongodb.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.wfr.projetoworkshopmongodb.domain.User;
 import com.wfr.projetoworkshopmongodb.repository.UserRepository;
+import com.wfr.projetoworkshopmongodb.services.exception.ObjectNotFoundException;
 
 @Service
 public class UserService {
@@ -18,4 +20,9 @@ public class UserService {
 		
 		return repo.findAll();
 	}	
+	
+	public User findById(String id) {
+		Optional<User> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+		}
 }
