cb01935031866cc3c535fb05a007b3a7eaddb425
Wanderson Fagundes Ramos <wandersonfagundesramos@gmail.com>
2021-06-17 14:20:56 -0300

    Deleção de usuário com DELETE

--- src/main/java/com/wfr/projetoworkshopmongodb/resources/UserResource.java
+++ src/main/java/com/wfr/projetoworkshopmongodb/resources/UserResource.java
@@ -34,18 +34,21 @@ public class UserResource {
 	
 	@RequestMapping(value = "/{id}", method=RequestMethod.GET)
 	public ResponseEntity<UserDTO> findById (@PathVariable String id) {
-		
 		User obj = service.findById(id);
-		
 		return ResponseEntity.ok().body(new UserDTO(obj));	
 	}
 	
 	@RequestMapping(method=RequestMethod.POST)
 	public ResponseEntity<Void> insert (@RequestBody UserDTO objDto) {
-		
 		User obj = service.fromDTO(objDto);
 		obj = service.insert(obj);
 		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
 		return ResponseEntity.created(uri).build();
+	}
+	
+	@RequestMapping(value = "/{id}", method=RequestMethod.DELETE)
+	public ResponseEntity<Void> delete (@PathVariable String id) {
+		service.delete(id);
+		return ResponseEntity.noContent().build();	
 	}
 }
