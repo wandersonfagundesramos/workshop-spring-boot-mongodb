7e4b8a8d6c0098eb96c5aa5518841cbbfb380f58
Wanderson Fagundes Ramos <wandersonfagundesramos@gmail.com>
2021-06-17 13:10:28 -0300

    Inserção de usuário com POST

--- src/main/java/com/wfr/projetoworkshopmongodb/services/UserService.java
+++ src/main/java/com/wfr/projetoworkshopmongodb/services/UserService.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.wfr.projetoworkshopmongodb.domain.User;
+import com.wfr.projetoworkshopmongodb.dto.UserDTO;
 import com.wfr.projetoworkshopmongodb.repository.UserRepository;
 import com.wfr.projetoworkshopmongodb.services.exception.ObjectNotFoundException;
 
@@ -25,4 +26,12 @@ public class UserService {
 		Optional<User> obj = repo.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
 		}
+	
+	public User insert (User obj) {
+		return repo.insert(obj);
+	}
+	
+	public User fromDTO (UserDTO objDTO) {
+		return new User(objDTO.getId(), objDTO.getName(), objDTO.getEmail());
+	}
 }
